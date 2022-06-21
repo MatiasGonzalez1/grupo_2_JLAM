e342b6095d10a7faf78933f20f8699885f39a25c
Leandro <leo170994@gmail.com>
2022-06-20 23:31:40 -0300

    refactor: html productCart with ejs

--- src/routes/product.routes.js
+++ src/routes/product.routes.js
@@ -2,15 +2,16 @@ const express = require('express');
 const routes = express.Router();
 const productController = require('../controller/productController');
 
+
+
 routes.get('/catalogue', productController.catalogo);
-routes.get('/product-cart', productController.carrito);
+routes.get('/product-cart/:id?', productController.carrito);
 routes.get('/product-detail/:id', productController.detalle);
 routes.get('/new-product', productController.nuevoProducto);
 routes.get('/update-product', productController.actualizarProducto);
 
 routes.get('/all-products', productController.cargarProductos); //ruta para cargar productos desde admin
 
-
-
+routes.delete('/product-cart/:id', productController.deleteCart);
 
 module.exports = routes;
