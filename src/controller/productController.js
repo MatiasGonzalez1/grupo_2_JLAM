4f71c4f56c5f17647196372d6bc261181cc9e73a
Chamorro Johanna <chamorrojohanna78@gmail.com>
2022-06-21 21:11:54 -0300

    feat:update product put route & dynamic view

--- src/controller/productController.js
+++ src/controller/productController.js
@@ -20,7 +20,7 @@ const productController = {
         let coincidencia = productos.find((producto) => { //filtro mis productos y busco el id
             return producto.id_producto === detalleId;
         });
-        console.log(coincidencia);
+
         res.render(path.join(__dirname, '../views/products/productDetail.ejs'), { coincidencia: coincidencia });
     },
 
@@ -28,15 +28,47 @@ const productController = {
         res.render(path.join(__dirname, '../views/products/newProduct.ejs'))
     },
 
-    actualizarProducto: (req, res) =>{
-        res.render(path.join(__dirname, '../views/products/updateProduct.ejs'))
+    verActualizarProducto: (req, res) =>{
+        const updateId =  Number(req.params.id);
+
+        let coincidencia = productos.find((producto) => { //filtro mis productos y busco el id
+            return producto.id_producto === updateId;
+        });
+
+        res.render(path.join(__dirname, '../views/products/updateProduct.ejs'), { coincidencia: coincidencia });
+    },
+
+    enviarActualizarProducto: (req, res) =>{
+        const idProducto = Number(req.body.fid);
+
+        let productosFilter = productos.filter((producto) => {
+            return producto.id_producto !== idProducto;
+        });
+
+        let formDataProduct = {
+            id_producto: idProducto,
+            nombre_producto: req.body.fnombre,
+            categoria: req.body.fcategoria,
+            anio_cosecha: req.body.fcoseAnio,
+            variedad: req.body.fvariedad,
+            crianza: req.body.fcrianza,
+            potencial_guarda: req.body.fguarda,
+            nota_cata: req.body.fnotacata,
+            imagen_producto: req.body.fprodfoto,
+            precio: req.body.fprecio,
+            stock: req.body.fstock,
+        }
+
+        productosFilter.push(formDataProduct);
+
+        let productosJson = JSON.stringify(productosFilter);
+        fs.writeFileSync(path.join(__dirname,'../models/data/products.json'), productosJson);
+
+        res.redirect('/product/all-products');
     },
 
     cargarProductos: (req, res) =>{
-        let archivoProductos = fs.readFileSync(path.join(__dirname, '../models/data/products.json'), { encoding: 'utf-8' });
-        let productos = JSON.parse(archivoProductos);
-        console.log(productos);
-        res.render(path.join(__dirname, '../views/adminArea.ejs'), { productos: productos });
+        res.render(path.join(__dirname, '../views/products/all-products.ejs'), { productos: productos });
     },
 
 }
