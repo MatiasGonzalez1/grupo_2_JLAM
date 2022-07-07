6497e95fa4d42a70164879e20ae20001ae7dc8e0
Chamorro Johanna <chamorrojohanna78@gmail.com>
2022-07-07 16:20:17 -0300

    feat:cart method using cookies

--- src/middleware/guestMiddleware.js
+++ src/middleware/guestMiddleware.js
@@ -3,7 +3,6 @@ function guestMiddleware (req, res, next){
         next();
     }else{
         res.redirect('/index');
-        window.alert("No tienes los permisos necesarios");
     }
 }
 module.exports = guestMiddleware;
