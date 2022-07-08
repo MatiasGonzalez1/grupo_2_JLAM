3befd8646688a4fbb32bccae953f5211686af7f8
Matias Gonzalez <mng_epson@hotmail.com>
2022-07-08 16:13:56 -0300

    feat: modify validations step 3

--- src/middleware/registerMidValidator.js
+++ src/middleware/registerMidValidator.js
@@ -1,4 +1,6 @@
 const { body } = require("express-validator");
+const path = require('path');
+
 
 const formValid = [
   body("nombre")
@@ -6,7 +8,11 @@ const formValid = [
     .withMessage("El campo nombre no puede estar vacio")
     .isLength({ min: 2 })
     .withMessage("El nombre debe de tener al menos dos carácteres"),
-  body("email").notEmpty().withMessage("Debe ingresar un email válido"),
+  body("email")
+    .notEmpty()
+    .withMessage("El campo email no puede estar vacio").bail()
+    .isEmail()
+    .withMessage("Debe de ingresar un email válido"),
   body("fechaNacimiento")
     .isDate()
     .withMessage("Debes tener 18 años o más para registrarte"),
@@ -20,19 +26,31 @@ const formValid = [
   body("repassword")
     // To delete leading and triling space
     .trim()
-  
-    // Custom validation
+    // Custom validation: pasa el valor tomado desde el body
     // Validate confirmPassword
     .custom(async (confirmPassword, {req}) => {
       const password = req.body.password
-  
       // If password and confirm password not same
       // don't allow to sign up and throw error
       if(password !== confirmPassword){
         throw new Error('La contraseña no coincide con la ingresada')
       }
     }),
-  body("profileImage"),
+  body("profileImage").custom((value, {req})=>{
+    let file = req.file;
+    let aceptedExt = ['.jpg', '.png', '.gif', '.webp'];
+    if(!file){
+      throw new Error('Tienes que subir una imagen')
+    } else{
+      let fileExt = path.extname(file.originalname)
+      if(!aceptedExt.includes(fileExt)){
+      throw new Error(`Las extensiones permitidas son ${aceptedExt.join(', ')}`);
+    }
+    }
+
+    
+    return true;
+  }),
 ];
 
 module.exports = formValid;
