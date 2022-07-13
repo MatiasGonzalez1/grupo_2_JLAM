36b1829b6cd6c76783cf1c02f40dbd6df298bc90
Chamorro Johanna <chamorrojohanna78@gmail.com>
2022-07-13 15:38:05 -0300

    update: user routes

--- src/routes/users.routes.js
+++ src/routes/users.routes.js
@@ -13,7 +13,7 @@ const loginValid = require('../middleware/loginMidValidator');
 routes.get('/edit-user/:id', userLogg,usersController.userData);
 routes.put('/edit-user', [userLogg, upload, updateUser], usersController.userEdit);
 routes.get('/login', guestMid, usersController.login);
-routes.post('/login', [guestMid, userLogg], loginValid, usersController.processLogin);
+routes.post('/login',  loginValid, usersController.processLogin);
 routes.get('/register', guestMid, usersController.registerView);
 routes.post('/register', [upload, guestMid], registerValid,usersController.register)
 routes.get('/logout',usersController.logout)
