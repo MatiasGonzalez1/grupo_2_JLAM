eb109f1dc808c203a9f864f411920c9766995076
Matias Gonzalez <mng_epson@hotmail.com>
2022-08-07 13:54:31 -0300

    feat add userCategory model

---
+++ src/database/models/UserCategory.js
@@ -0,0 +1,29 @@
+module.exports = (sequelize, DataTypes)=>{
+
+
+    const UserCategory = sequelize.define("userCategories", {
+        idUserCategory: {
+            autoIncrement: true,
+            primaryKey: true,
+            type:DataTypes.INTEGER
+        },
+            userCategoryName:{
+            allowNull: false,
+            type: DataTypes.STRING
+        }
+     }, 
+        {
+        tableName: "userCategory",
+        timestamps: false
+    });
+
+    UserCategory.associate = models =>{
+        UserCategory.hasMany(models.user,
+            {
+                as: "user",
+                foreignKey: "userCategory"
+            }
+        );
+    };
+    return UserCategory;
+}
