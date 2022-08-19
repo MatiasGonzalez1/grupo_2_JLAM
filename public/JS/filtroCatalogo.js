7d83bc061d8e1aea5e64647d13dc92e4186e70db
Matias Gonzalez <mng_epson@hotmail.com>
2022-08-19 11:31:32 -0300

    add checkbox checked

--- public/JS/filtroCatalogo.js
+++ public/JS/filtroCatalogo.js
@@ -7,12 +7,19 @@ show.style.display === 'none' ? show.style.display = 'block' : show.style.displa
 });
 
 //cambiando el rango de precio
+const precio = []
 const range = document.querySelector('#precio');
-range.addEventListener('change', ()=>{
+range.addEventListener('change', (e)=>{
         let outvol = document.getElementById('outvol');
             outvol.innerHTML = 'Hasta ' + range.value
+            let guard = e.target.value
+            precio.push(guard)
 })
 
+// trayendo productos por precio
+
+
+//capturando el valor del checkbox
 const ti = document.getElementById('tintos');
 const bl = document.getElementById('blancos');
 const es = document.getElementById('especiales');
@@ -21,23 +28,41 @@ const esp = document.getElementById('espumantes');
 const catFil = [
     ti, bl, es, esp
 ]
-
 catFil.forEach(categoria => {
-    categoria.addEventListener('click', (e)=>{
+    categoria.addEventListener('change', ()=>{
     if(categoria.checked){
-     let enter = e.target.value
-     filtro.push(enter)
-     console.log(filtro)
-    } else if(categoria.unchecked){
-    filtro = []
-    }
+        categoria.setAttribute('checked', 'checked')
+    } else if(categoria.checked != true){
+    categoria.removeAttribute('checked')
+    } 
 })
 });
 
-// trayendo productos por precio
 
-//capturando el valor del checkbox
-const filtro = []
+
+//capturando el valor del checkbox variedad
+const ma = document.getElementById('malbec');
+const mer = document.getElementById('merlot');
+const pin = document.getElementById('pinotNoir');
+const cab = document.getElementById('cabernet');
+const sy = document.getElementById('syrah');
+const tem = document.getElementById('tempranillo');
+const char = document.getElementById('chardonnay');
+
+const varFil = [
+    ma, mer, pin, cab, sy, tem, char
+]
+
+varFil.forEach(variedad => {
+    variedad.addEventListener('change', ()=>{
+    if(variedad.checked){
+       // let enter = e.target.value
+        variedad.setAttribute('checked', 'checked')
+    } else if(variedad.checked != true){
+    variedad.removeAttribute('checked')
+    } 
+})
+});
 
 // const todos = document.querySelectorAll('[type="checkbox"]');
 
@@ -68,7 +93,6 @@ const filtro = []
 //     })
 // })
 
-console.log(filtro)
 }
 
 )
