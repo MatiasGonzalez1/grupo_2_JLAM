d57b8eeaef8decdfbdc325524afe8baee7668351
Matias Gonzalez <mng_epson@hotmail.com>
2022-08-19 15:06:49 -0300

    capture input checkbox

--- public/JS/filtroCatalogo.js
+++ public/JS/filtroCatalogo.js
@@ -15,6 +15,7 @@ range.addEventListener('change', (e)=>{
             let guard = e.target.value
             precio.push(guard)
 })
+console.log(precio)
 
 // trayendo productos por precio
 
@@ -25,26 +26,26 @@ const bl = document.getElementById('blancos');
 const es = document.getElementById('especiales');
 const esp = document.getElementById('espumantes');
 
-const catFil = [
-    ti, bl, es, esp
-]
-catFil.forEach(categoria => {
-    categoria.addEventListener('change', ()=>{
-    if(categoria.checked){
-        categoria.setAttribute('checked', 'checked')
-    } else if(categoria.checked != true){
-    categoria.removeAttribute('checked')
-    } 
+const catFil = [ti, bl, es, esp]
+let cat = []
+for(let i = 0; i < catFil.length; i++){
+    const elemento = catFil[i];
+    catFil[i].addEventListener('change', (e)=>{
+    if(catFil[i].checked == true && !cat.includes(catFil[i])){
+    cat.push(elemento)
+    } else if(catFil[i].checked == false){
+        cat.pop(elemento)
+    }
+    console.log(cat)
 })
-});
-
+};
 
 
 //capturando el valor del checkbox variedad
 const ma = document.getElementById('malbec');
 const mer = document.getElementById('merlot');
 const pin = document.getElementById('pinotNoir');
-const cab = document.getElementById('cabernet');
+const cab = document.getElementById('cabernetSauvignon');
 const sy = document.getElementById('syrah');
 const tem = document.getElementById('tempranillo');
 const char = document.getElementById('chardonnay');
@@ -52,47 +53,19 @@ const char = document.getElementById('chardonnay');
 const varFil = [
     ma, mer, pin, cab, sy, tem, char
 ]
+let varf = [];
 
-varFil.forEach(variedad => {
-    variedad.addEventListener('change', ()=>{
-    if(variedad.checked){
-       // let enter = e.target.value
-        variedad.setAttribute('checked', 'checked')
-    } else if(variedad.checked != true){
-    variedad.removeAttribute('checked')
-    } 
+for(let i = 0; i < varFil.length; i++){
+    const el = varFil[i];
+    varFil[i].addEventListener('change', (e)=>{
+    if(varFil[i].checked == true && !varf.includes(varFil[i])){
+    varf.push(el)
+    } else if(varFil[i].checked == false){
+        varf.pop(el)
+    }
+    console.log(varf)
 })
-});
-
-// const todos = document.querySelectorAll('[type="checkbox"]');
-
-// for(let i = 0, len = todos.length; i<len ; i++){
-//     todos[i].addEventListener('change', ()=>{
-//         if (todos[i].checked == true) {
-//             console.log('esta checkeado')
-//             filtro.push(todos[i].name)}
-
-//     })
-// }
-
-// todos.addEventListener('change', ()=>{
-//     todos.forEach(valor =>{
-//              if(valor.checked){
-//                 console.log('chequeado')
-//                 filtro.push(valor.defaultValue)
-//              }
-//             })
-// });
-
-// todos.forEach(valor =>{
-//     valor.addEventListener('change', (e)=>{
-//          if(valor.checked){
-//             let s = e.target.value
-//         filtro.push(s)
-//          }
-//     })
-// })
+};
 
 }
-
 )
