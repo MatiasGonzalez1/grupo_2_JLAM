701c732aef980f2dd3e2f3b4436793e25c45776d
Chamorro Johanna <chamorrojohanna78@gmail.com>
2022-08-20 00:36:13 -0300

    feat: catalogue filter method fixed

--- public/JS/filtroCatalogo.js
+++ public/JS/filtroCatalogo.js
@@ -6,66 +6,111 @@ let show = document.getElementById('show');
 show.style.display === 'none' ? show.style.display = 'block' : show.style.display ='none';
 });
 
+
+const formatter = new Intl.NumberFormat("es-AR", { 
+    style: "decimal",
+    minimumFractionDigits: 2,
+});
+
+
+//tomo todos los inputs que contienen los tipos de vinos
+let filterType = document.querySelectorAll("#filterType")
+let divSearch = document.getElementById("divSearch")
+
+
+//declaramos una para asignarle lo que queremos filtrar
+let params = {
+    type: [],
+    price: null,
+    reset: 0,
+}
+
+//recorro el array y le asigno el evento change
+for (var i = 0, len = filterType.length; i < len; i++) {
+    filterType[i].addEventListener('change', function(e){
+        //selecciono el elemnto especifico al que
+        let valueTarget = e.currentTarget
+
+        // chequeamos que el valor de input no exista en nuestro array 
+        if (!params.type.includes(valueTarget.value) && valueTarget.checked == true){
+            params.type.push(valueTarget.value);
+
+        }else if(params.type.includes(valueTarget.value)&& valueTarget.checked == false ){
+           params.type = params.type.filter(function (e){
+             return e != valueTarget.value;
+           })
+        }
+        filter();
+    });
+}
+
+
+
 //cambiando el rango de precio
-const precio = []
 const range = document.querySelector('#precio');
 range.addEventListener('change', (e)=>{
         let outvol = document.getElementById('outvol');
             outvol.innerHTML = 'Hasta ' + range.value
-            let guard = e.target.value
-            precio.push(guard)
+            //tomo el valor actual y lo asigno a mi variable precio
+            params.price = range.value; 
+            filter();
 })
-console.log(precio)
-
-// trayendo productos por precio
 
 
-//capturando el valor del checkbox
-const ti = document.getElementById('tintos');
-const bl = document.getElementById('blancos');
-const es = document.getElementById('especiales');
-const esp = document.getElementById('espumantes');
 
-const catFil = [ti, bl, es, esp]
-let cat = []
-for(let i = 0; i < catFil.length; i++){
-    const elemento = catFil[i];
-    catFil[i].addEventListener('change', (e)=>{
-    if(catFil[i].checked == true && !cat.includes(catFil[i])){
-    cat.push(elemento)
-    } else if(catFil[i].checked == false){
-        cat.pop(elemento)
+const filter = ()=>{
+    const url = new URL("http://localhost:3000/product/catalogue/filter");
+    // pregunto si mis atributos (tipo, precio, variedad)contienen algo
+    if (params.price) {
+        url.searchParams.append("price", params.price);
     }
-    console.log(cat)
-})
-};
-
-
-//capturando el valor del checkbox variedad
-const ma = document.getElementById('malbec');
-const mer = document.getElementById('merlot');
-const pin = document.getElementById('pinotNoir');
-const cab = document.getElementById('cabernetSauvignon');
-const sy = document.getElementById('syrah');
-const tem = document.getElementById('tempranillo');
-const char = document.getElementById('chardonnay');
-
-const varFil = [
-    ma, mer, pin, cab, sy, tem, char
-]
-let varf = [];
-
-for(let i = 0; i < varFil.length; i++){
-    const el = varFil[i];
-    varFil[i].addEventListener('change', (e)=>{
-    if(varFil[i].checked == true && !varf.includes(varFil[i])){
-    varf.push(el)
-    } else if(varFil[i].checked == false){
-        varf.pop(el)
+    if (params.type.length) {
+        url.searchParams.append("type", params.type);
     }
-    console.log(varf)
-})
-};
+    if (params.reset) {
+        url.searchParams.append("filter", params.reset);
+    }
+
+
+    fetch(url)
+    .then(function(response) {
+        return response.json();
+    })
+    .then(function(productos) { 
+       
+        let productFilter = productos.data;
+
+        //limpio el div que contiene todos los bloques de productos
+        divSearch.innerHTML = "";
+
+        let setArticle = '';
+
+        //recorro mi array de productos y genero cada bloque 
+        productFilter.forEach(producto => {  
+            setArticle+=   
+
+            `  <article class="cata__products">
+                    <img class="cataimg__product" src="/img/productImg/${producto.productImg}" alt="aqui va la foto">
+                    <p class="cataname__product"> ${producto.productName}</p>
+                    <p class="cataprecio__product">$ ${ formatter.format(producto.productPrice)}</p>
+                    
+                    <form action="/product/product-cart/${ producto.idProduct}" method="post">
+                        <div class="container__buttons">
+                            <button type="submit" class="add-to-cart"><i class="fa-solid fa-cart-shopping"></i></button>
+                            <a class="cata__eye" href="/product/product-detail/${ producto.idProduct}" target=""><i class="fa-regular fa-eye"></i></a>
+                        </div>
+                    </form>
+                </article>` 
+        });
+
+        //lo seteo en mi div contenedor
+        divSearch.innerHTML = setArticle;
+       
+    })
+    }
+
+
+
 
 }
 )
--- public/JS/navSearch.js
+++ public/JS/navSearch.js
@@ -2,12 +2,6 @@ window.onload = () => {
     const inputSearch = document.getElementById("search-input");
     const formSearch = document.getElementById("searchForm");
 
-
-    const formatter = new Intl.NumberFormat("es-AR", { 
-        style: "decimal",
-        minimumFractionDigits: 2,
-    });
-
     inputSearch.addEventListener("keypress", function(event) {
         
         if (event.key === "Enter") {
