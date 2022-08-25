3dc71866247a6844c2d781472ac5e21f1c32005a
Matias Gonzalez <mng_epson@hotmail.com>
2022-08-25 08:39:47 -0300

    refactor: modify & finish front login validator

--- public/JS/loginValidator.js
+++ public/JS/loginValidator.js
@@ -1,4 +1,5 @@
 window.addEventListener("load", (e) => {
+  //capturando inputs y form
   let inputEmail = document.querySelector("#email");
   let inputPass = document.querySelector("#password");
   let formulario = document.querySelector("form");
@@ -6,71 +7,60 @@ window.addEventListener("load", (e) => {
   let arrInp = [inputEmail, inputPass];
 
   formulario.addEventListener("submit", (e) => {
-    let errores = 0;
     arrInp.forEach((inp) => {
-      if (inp.value.length == 0) {
+      if (inp.value.length < 1) {
         inp.classList.add("is-invalid");
-        
-        errores++
+        e.preventDefault();
       }
       if (inp.value != "") {
         inp.classList.remove("is-invalid");
       }
     });
+  });
 
-    if(errores > 0){
-        e.preventDefault();
+  inputEmail.addEventListener("blur", () => {
+    let mostrar = document.querySelector("#er");
+    if (inputEmail.value == "") {
+      inputEmail.classList.add("is-invalid");
+      mostrar.innerHTML = "El campo no puede estar vacío";
     }
   });
 
-  
-    inputEmail.addEventListener('input',()=>{
-        let mostrar = document.querySelector('#er')
-        const emailValido = email => {
-            return /^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(email);
-          }
-        if(inputEmail.value == ''){
-            inputEmail.classList.add("is-invalid");
-                mostrar.innerHTML = 'El campo no puede estar vacío'
-                return
-            }
-         else if(inputEmail.value.length < 2){
-            mostrar.innerHTML = 'El campo debe tener más de dos carácteres'
-            return
-        }
-        else if(!emailValido(inputEmail.value)){
-            mostrar.innerHTML = 'Debe de ingresarse un email válido'
-            return
-        }
-       // else if() aca voy a comprobar si es un email valido
-         
-            // else if (inputEmail.value != ''){
-            //     inputEmail.classList.remove("is-invalid");
-            //     document.querySelector('#er').innerHTML = ''
-            // }
+  inputEmail.addEventListener("input", () => {
+    let mostrar = document.querySelector("#er");
+    const emailValido = (email) => {
+      return /^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(email);
+    };
+    if (inputEmail.value.length < 2) {
+      inputEmail.classList.add("is-invalid");
+      mostrar.innerHTML = "El campo debe tener más de dos carácteres";
+      return;
+    } else if (!emailValido(inputEmail.value)) {
+      inputEmail.classList.add("is-invalid");
+      mostrar.innerHTML = "Debe de ingresarse un email válido";
+    } else {
+      inputEmail.classList.remove("is-invalid");
+      mostrar.innerHTML = "";
+    }
+  });
 
-            else if (inputEmail.value != ''){
-                inputEmail.classList.remove('is-invalid')
-                mostrar.innerHTML = ''
-            }
-  })
-
-  inputPass.addEventListener('input',()=>{
-  let mostrar2 = document.querySelector('#er2')
-  if(inputPass.value == ''){
-    inputPass.classList.add("is-invalid");
-        mostrar2.innerHTML = 'El campo no puede estar vacío'
-        return
+  inputPass.addEventListener("blur", () => {
+    let mostrar2 = document.querySelector("#er2");
+    if (inputPass.value == "") {
+      inputPass.classList.add("is-invalid");
+      mostrar2.innerHTML = "El campo no puede estar vacío";
+    } 
+  });
+  inputPass.addEventListener("input", () => {
+    let mostrar2 = document.querySelector("#er2");
+    if (inputPass.value.length < 8) {
+      inputPass.classList.add("is-invalid");
+      mostrar2.innerHTML = "El campo debe tener más de ocho carácteres";
+    } else {
+      inputPass.classList.remove("is-invalid");
+      mostrar2.innerHTML = "";
     }
-    else if(inputPass.value.length < 8){
-        mostrar2.innerHTML = 'El campo debe tener más de ocho carácteres'
-    }
-    else{
-        inputPass.classList.remove('is-invalid')
-        mostrar2.innerHTML = ''
-    }
-    
-})
+  });
 
   //cierre del load
 
