47cdc7c45a62db14e8cdc3e861a188249aa5aeaa
Leandro <leo170994@gmail.com>
2022-09-11 20:10:14 -0300

    feat: new routes for API

---
+++ src/controller/api/usersAPIController.js
@@ -0,0 +1,17 @@
+const db = require('../../database/models');
+const {Op} = require('sequelize');
+
+
+const userAPIController = {
+
+    'loadUsers': (req, res) =>{ //listado de usuarios
+        
+    },
+
+    'userData': (req, res) =>{ //datos de usuario por id
+        
+    }
+
+};
+
+module.exports = userAPIController;
--- src/routes/index.routes.js
+++ src/routes/index.routes.js
@@ -6,7 +6,8 @@ routes.use('/', require('./main.routes'));
 routes.use('/product', require('./product.routes'));
 routes.use('/users', require('./users.routes'));
 routes.use('/content', require('./content.routes'));
+routes.use('/api', require('./api/api.routes.js'));
 
 
 
-module.exports = routes
+module.exports = routes;
