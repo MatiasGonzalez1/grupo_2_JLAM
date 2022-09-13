75a4c17e8174da52751490c36d894f84d4b69e83
Matias Gonzalez <mng_epson@hotmail.com>
2022-09-13 19:04:14 -0300

    feat: add productsAPIController

--- src/controller/api/productsAPIController.js
+++ src/controller/api/productsAPIController.js
@@ -1,11 +1,34 @@
 const db = require('../../database/models');
 const {Op} = require('sequelize');
 
-const productAPIController = {
+let dataSet = (respuesta, array) => {
+    respuesta.forEach((producto) => {
+      array.push({  
+        id: producto.idProduct,
+        name:producto.productName,
+        description:producto.productDescription,
+        category: producto.idProductCategory,
+        detail: `http://localhost:3001/api/product/${producto.idProduct}`,
+      });
+    });
+  };
 
-    'loadProducts': (req, res)=>{ //listado de productos
-
-    },
+const productsAPIController = {
+    loadProducts: (req, res)=>{ //listado de productos   
+        db.Product.findAll()
+            .then((products)=>{
+            // Creo un array que contendrá a cada usuario
+                let datos = [];
+                dataSet(products, datos);   
+                return res.json({
+                    status: 200,
+                    count: products.length,
+                    countByCategory: '',
+                    products:datos
+                })
+            })
+            ;
+},  
 
     'ProductData': (req, res)=>{ //datos de producto por id
         
@@ -13,4 +36,4 @@ const productAPIController = {
 
 }
 
-module.exports = productAPIController
+module.exports = productsAPIController
