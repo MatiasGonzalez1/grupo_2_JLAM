c3b7d694397ebc4c1b1afaab10d5233b6c8b59e6
Amalita9 <amalita9421@gmail.com>
2022-09-19 11:53:05 -0300

    refactor: change vs react add folders

--- tr-dashboard/src/App.js
+++ tr-dashboard/src/App.js
@@ -1,5 +1,10 @@
 import Sidebar from "./Components/Sidebar/Sidebar";
 import './Components/Sidebar/sidebar.css';
+import { Switch, Route, Redirect } from "react-router-dom";
+import Usuarios from "./Components/Usuarios/Usuarios";
+import Productos from "./Components/Productos/Productos";
+import Categorias from "./Components/Categorias/Categorias";
+import Index from "./views/Index"
 
 function App() {
   return (
@@ -7,6 +12,17 @@ function App() {
        {/* <!-- Sidebar -->  */}
        <Sidebar />
        {/* <!-- End of Sidebar --> */}
+
+       <Switch>
+          <Route exact path="/" component={Index} />
+          <Route path="/usuarios" component={Usuarios} />
+          <Route path="/productos" component={Productos} />
+          <Route path="/categorias" component={Categorias} />
+          
+          <Route>
+            <Redirect to="/?error=pagina_no_encontrada" />
+          </Route>
+        </Switch>
     </div>
   );
 }
