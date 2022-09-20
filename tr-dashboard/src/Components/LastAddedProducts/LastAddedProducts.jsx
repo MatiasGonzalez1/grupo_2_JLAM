c190860ee7f87defa1ce7a7d73feba1ea97ac418
Chamorro Johanna <chamorrojohanna78@gmail.com>
2022-09-20 19:17:27 -0300

    feat:last added products component & api created

--- tr-dashboard/src/Components/LastAddedProducts/LastAddedProducts.jsx
+++ tr-dashboard/src/Components/LastAddedProducts/LastAddedProducts.jsx
@@ -1,7 +1,23 @@
-import React from 'react'; 
+import React, { useEffect, useState } from 'react'; 
+import Formatter from '../../utils/Formatter.js';
 import "./LastAddedProducts.css";
 
 const LastAddedProducts = ()=>{
+    const [products, setProducts] = useState([]);
+
+    const getLastAdded = async()=>{
+        try {
+            const result = await fetch("http://localhost:3001/api/last-added")
+            const productsJson = await result.json();
+            setProducts(productsJson);
+        } catch (error) {
+            console.log(error);
+        }
+    }
+    useEffect(() => {
+        getLastAdded();
+    }, []);
+
     return <div className='last-added-container'>
         <div className='last-main-groups last-added-title'>
             <h3>Últimos productos agregados</h3>
@@ -16,11 +32,14 @@ const LastAddedProducts = ()=>{
         </div>
         <div className='last-main-groups'>
             <div className='last-added-columns border-none'>
-                <>
-                    <p className='product-detail'>Colección Terra Rossa Malbec</p>
-                    <p className='product-detail'>$5.460,00</p>
-                    <p className='product-detail'>100</p>
-                </>
+                {products.map(product => (
+                    <>
+                        <p className='product-detail'key={product.idProduct}>{product.productName}</p>
+                        <p className='product-detail'>${Formatter.format(product.productPrice)}</p>
+                        <p className='product-detail'>{product.productStock}</p>
+                    </>
+                ))
+                }
             </div>
         </div>
     </div>
