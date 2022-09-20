b936f209fbfa2f3086dbd1d5cb84211a54dc865e
Chamorro Johanna <chamorrojohanna78@gmail.com>
2022-09-20 16:22:05 -0300

    feat: index layout html & css structure

---
+++ tr-dashboard/src/views/Home.jsx
@@ -0,0 +1,47 @@
+import React from "react";
+import "./Home.css";
+// import Sidebar from "../Components/Sidebar/Sidebar";
+
+function home() {
+
+    return <div className="content-wrapper">
+        <div className="color">
+          <p>sda</p>
+        </div>
+        <div className="card-content">
+          <div className="example2">
+          </div>
+          <div className="example2">
+          </div>
+          <div className="example2">
+          </div>
+        </div>
+        <div className="other-content">
+          <div className="example">
+          
+          </div>
+          <div className="categories-cards">
+            <div className="example3">
+            
+            </div>
+            <div className="example3">
+            
+            </div>
+            <div className="example3">
+            
+            </div>
+            <div className="example3">
+            
+            </div>
+            <div className="example3">
+            
+            </div>
+          </div>
+        </div>
+    
+        {/* <!-- Main Content --> */}
+    
+    </div>
+  }
+
+export default home ;
