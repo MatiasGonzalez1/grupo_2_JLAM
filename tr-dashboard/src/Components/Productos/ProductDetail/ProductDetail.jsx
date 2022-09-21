949bb0a88e034d275f2bdf73fb3d6779bd31a403
Chamorro Johanna <chamorrojohanna78@gmail.com>
2022-09-20 21:12:10 -0300

    feat: related items & product detail component structure

---
+++ tr-dashboard/src/Components/Productos/ProductDetail/ProductDetail.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+import "./ProductDetail.css";
+
+function ProductDetail() {
+    return <div className="products-container">
+
+                <div className="product-detail-section">
+                </div>
+                <div className="products-related-section"> 
+                    <div className="products-element"> 
+                    </div>
+                    <div className="products-element"> 
+                        {/* hacer el componente productos relacionados */}
+                    </div>
+                </div>
+    </div>
+    
+  }
+
+export default ProductDetail ;
--- tr-dashboard/src/Components/Productos/Products.jsx
+++ tr-dashboard/src/Components/Productos/Products.jsx
@@ -1,24 +1,15 @@
 import React from "react";
+import ProductDetail from "./ProductDetail/ProductDetail";
 import "./Products.css";
 
 function Products() {
     return <div className="content-wrapper">
+        {/* banner con los datos del admin */}
         <div className="color">
         </div>
 
-        <div className="products-container">
-
-            {/* componente con los detalles */}
-            <div className="product-detail-section">
-            </div>
-            <div className="products-related-section"> 
-              <div className="products-element"> 
-              </div>
-              <div className="products-element"> 
-                {/* hacer el componente productos relacionados */}
-              </div>
-            </div>
-        </div>
+        {/* componente con los detalles */}
+        <ProductDetail/>
         
     </div>
   }
