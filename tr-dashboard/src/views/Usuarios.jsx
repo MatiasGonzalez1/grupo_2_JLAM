82031a223deaac5b1f04134de43b6d0da9de9c9d
Chamorro Johanna <chamorrojohanna78@gmail.com>
2022-09-22 17:45:39 -0300

    feat:last user created component

--- tr-dashboard/src/views/Usuarios.jsx
+++ tr-dashboard/src/views/Usuarios.jsx
@@ -1,10 +1,26 @@
-import React from "react";
+import React, { useEffect , useState } from "react";
 import LastUserDetail from "../Components/Usuarios/LastUserDetail/LastUserDetail";
 import './Usuarios.css';
 
 function Usuarios() {
-  return (
-    <div className="users-main-container">
+  const [lastCreated, setLastCreated] = useState({});
+  const [users, setUsers] = useState([]);
+  
+  const getUsers = async()=>{
+    try {
+        const result = await fetch(`http://localhost:3001/api/users`)
+        const usersJson = await result.json();
+        setUsers(usersJson);
+        setLastCreated(usersJson.lastUser);
+    } catch (error) {
+        console.log(error);
+    }
+  }
+  useEffect(() => {
+    getUsers();
+  }, []);
+  
+  return <div className="users-main-container">
         <div className="all-users">
           <h2>titulo + tarjetas de usuarios con paginado</h2>
           <div className="user-card-container">
@@ -17,10 +33,9 @@ function Usuarios() {
         </div>
         <div className="last-users-container">
             <div className="recent-users"></div>
-            <LastUserDetail/>
+            <LastUserDetail lastUser={lastCreated} />
         </div>
     </div>
-  )
 }
 
-export default Usuarios
+export default Usuarios;
