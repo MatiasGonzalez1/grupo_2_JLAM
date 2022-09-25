e976766e538845e18a9dceccd18faf64c88a4654
Amalita9 <amalita9421@gmail.com>
2022-09-25 19:15:09 -0300

    refactor:validation checkout

--- public/JS/checkoutValidation.js
+++ public/JS/checkoutValidation.js
@@ -5,6 +5,8 @@ window.addEventListener('load', () => {
     const paymentData = document.getElementById('paymentData');
     const shippingForm = document.getElementById('shippingForm');
     const paymentForm = document.getElementById('paymentForm');
+   
+
     
     paymentVisible.addEventListener("click", () => {
       paymentData.classList.toggle("disabled");
@@ -30,6 +32,11 @@ window.addEventListener('load', () => {
     let cityCheck = document.querySelector("select.checkoutCity");
     let addressCheck = document.querySelector("input.checkoutAddress");
     let floorCheck = document.querySelector("input.checkoutFloor");
+    let numberCheck = document.querySelector(".card__number");
+    let nameSurCheck = document.querySelector(".name__user");
+    let dateCheck = document.querySelector(".date__card");
+    let codeCheck = document.querySelector(".code__segurity");
+    let dniCheck = document.querySelector(".dni__user");
 
     //inputs medio de pago - pendientes
     let errores = {};
@@ -141,10 +148,100 @@ window.addEventListener('load', () => {
         }
     })
 
+    //Validacion para el numero
+    numberCheck.addEventListener('input', () =>  {
+        if (numberCheck.value == "") {
+            errores.numberCheck = "Este campo es obligatorio"
+            numberCheck.classList.add('input-warning');
+            errorWarning(numberCheck, errores.numberCheck);
+        } else if (numberCheck.value.length < 7) {
+            errores.numberCheck = "El nombre debe contener al menos 3 carácteres"
+            numberCheck.classList.add('input-warning');
+            errorWarning(numberCheck, errores.numberCheck);
+        } else {
+            delError(numberCheck)
+        }
+
+    });
+
+    //Validacion para el nombre y apellido
+    nameSurCheck.addEventListener('input', function () {
+        if (nameSurCheck.value == "") {
+            errores.nameSurCheck = "El campo nombre y apellido no puede estar vacío"
+            nameSurCheck.classList.add('input-warning');
+            errorWarning(nameSurCheck, errores.nameSurCheck);
+        } else if (nameSurCheck.value.length < 5) {
+            errores.nameSurCheck = "Este campo debe tener al menos cinco caracteres y no pueden ser especiales"
+            nameSurCheck.classList.add('input-warning');
+            errorWarning(nameSurCheck, errores.nameSurCheck);
+        } else {
+            delError(nameSurCheck);
+        }
+
+    });
+
+    //Validacion para la fecha de expiracion
+    dateCheck.addEventListener('change', function () {
+        let dateExpi = "2022/01/01"
+        let dateExpiracion = "2018/01/01"
+        if (dateCheck.value == "") {
+            errores.dateCheck = "Debes seleccionar una fecha"
+            dateCheck.classList.add('input-warning');
+            errorWarning(dateCheck, errores.dateCheck);
+        } else if (dateCheck.value > dateExpi || dateCheck.value < dateExpiracion) {
+            errores.dateCheck = "Debes ingresar una tarjeta válida"
+            dateCheck.classList.add('input-warning');
+            errorWarning(dateCheck, errores.dateCheck);
+        } else {
+            delError(dateCheck)
+        }
+    });
+
+    //Validacion para el codigo
+    codeCheck.addEventListener('input', function () {
+        if (codeCheck.value == "") {
+            errores.codeCheck = "Este campo es obligatorio"
+            codeCheck.classList.add('input-warning');
+            errorWarning(codeCheck, errores.codeCheck)
+        } else if (codeCheck.value.length < 3) {
+            errores.codeCheck = "Este campo debe tener tres carácteres"
+            codeCheck.classList.add('input-warning');
+            errorWarning(codeCheck, errores.codeCheck)
+        } else {
+            delError(codeCheck)
+        }
+
+    });
+
+    //Validacion para el dni del titular
+    dniCheck.addEventListener('input', function () {
+        if (dniCheck.value == "") {
+            errores.dniCheck = "Este campo es obligatorio"
+            dniCheck.classList.add('input-warning');
+            errorWarning(dniCheck, errores.dniCheck)
+        } else if (dniCheck.value.length < 7) {
+            errores.dniCheck = "Este campo debe tener mas de siete carácteres"
+            dniCheck.classList.add('input-warning');
+            errorWarning(dniCheck, errores.dniCheck)
+        } else {
+            delError(dniCheck)
+        }
+
+    });
+
+    let pass;
     form.addEventListener('submit', (e) => {
         e.preventDefault();
-        if (Object.keys(errores).length < 1) {
+        if (Object.keys(errores).length >= 1) {
+            pass = false
+            e.preventDefault()                
+        } else {
+            pass = true
             form.submit();
         }
+        
+         
     })
+    
+    
 })
