0bb5916b43c2cd556aa50fd240346d68829c5024
Amalita9 <amalita9421@gmail.com>
2022-09-26 19:26:30 -0300

    refactor: css mobile usuarios/layout

--- tr-dashboard/src/Components/Productos/ProductDetailData/ProductDetailData.jsx
+++ tr-dashboard/src/Components/Productos/ProductDetailData/ProductDetailData.jsx
@@ -1,6 +1,5 @@
 import React, { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
-// import Img from "../../../Assets/images/ProductImg/blend.png";
 import Formatter from '../../../utils/Formatter.js';
 import "./ProductDetailData.css";
 
